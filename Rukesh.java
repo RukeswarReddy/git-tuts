9a5e39cd4866ae96a5b79fdb66192ffd43088a59
Your Name <you@example.com>
2023-07-03 11:30:07 +0530

    commiting the Rukesh java and Rukesh py

---
+++ Rukesh.java
@@ -0,0 +1,10 @@
+public class Rukesh
+{
+    public static void main(String[] args)
+    {
+        int a = 20;
+        String b = "Rukku";
+        System.out.println(b);
+        System.out.println(a);
+    }
+}
